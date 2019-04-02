6ee55095b22582dc1b72117cd1919d9048de83be
nathansandi <nathan.sandi@gmail.com>
2019-04-02 13:25:34 +0200

    1.1
    Exercise from udemy course: Hibernate one-to-many relationship
    
    Coded by Nathanyel Sandi
    Created by Chad Darby

---
+++ hibernate-one-to-many/src/com/luv2code/hibernate/demo/DeleteCoursesDemo.java
@@ -0,0 +1,43 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Course;
+import com.luv2code.hibernate.demo.entity.Instructor;
+import com.luv2code.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteCoursesDemo {
+
+	public static void main(String[] args) {
+		//create session factory
+		SessionFactory factory = new Configuration().
+									configure("hibernate.cfg.xml").
+									addAnnotatedClass(Instructor.class).
+									addAnnotatedClass(InstructorDetail.class).
+									addAnnotatedClass(Course.class).
+									buildSessionFactory();
+		//create session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//start
+			session.beginTransaction();
+			//get a course
+			int theId=1;
+			//delete
+			Course tempCourse = session.get(Course.class, theId);
+			session.delete(tempCourse);
+			//commit
+			session.getTransaction().commit();
+
+		}
+		
+		finally {
+			session.close();
+			factory.close();
+		}
+	}
+
+}
